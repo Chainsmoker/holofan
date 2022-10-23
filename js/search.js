992e3c114ed9ec9d19c650a320224686a88f580c
DESKTOP-6L08L2B\MystT <hackryme@gmail.com>
2022-10-23 15:52:25 -0600

    boton busqueda
    -boton de busqueda es clickeable

--- js/search.js
+++ js/search.js
@@ -1,9 +1,21 @@
 const search = document.getElementById('search-bar');//esta es la barra de busqueda
 const searchButtom = document.getElementById('search-buttom'); //el boton de busqueda
+let wasClick = false; //el boton fue presionado?;
+
 //aplicar estilo al tocar
 function onTouchSearch(){
-    search.style.display = 'block';
-    search.style.visibility = 'visible';
+
+    if (wasClick) {
+      search.style.display = "none";
+      search.style.visibility = "hidden";
+      wasClick = !wasClick;
+      console.log(wasClick + " fue escondido");
+    } else {
+      search.style.display = "block";
+      search.style.visibility = "visible";
+      wasClick = !wasClick;
+      console.log(wasClick + " fue mostrado");
+    }
 }
 
  searchButtom.addEventListener('click', onTouchSearch);
