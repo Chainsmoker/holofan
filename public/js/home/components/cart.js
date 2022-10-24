eca0f992c2138a29d60a2e403571510230cb5ab6
DESKTOP-6L08L2B\MystT <hackryme@gmail.com>
2022-10-23 21:48:23 -0600

    TT.TT
    demasiado para ser descrito

--- public/js/home/components/cart.js
+++ public/js/home/components/cart.js
@@ -1,13 +1,11 @@
-document.addEventListener("DOMContentLoaded", ()=>{
-  let cart = document.querySelector("#cart")
-  let cartCon = document.querySelector(".cart-shop--container")
-  let closeCart = document.querySelector("#close-cart")
-  cart.addEventListener("click", ()=>{
-    cartCon.classList.add("open")
-
+document.addEventListener("DOMContentLoaded", () => {
+  let cart = document.querySelector("#cart");
+  let cartCon = document.querySelector(".cart-shop--container");
+  let closeCart = document.querySelector("#close-cart");
+  cart.addEventListener("click", () => {
+    cartCon.classList.add("open");
   });
-  closeCart.addEventListener("click", ()=>{
+  closeCart.addEventListener("click", () => {
     cartCon.classList.remove("open");
-
-  })
-})
+  });
+});
