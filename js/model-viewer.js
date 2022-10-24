79c1d68fbc55d85a1055bf161b8027881e941500
DESKTOP-6L08L2B\MystT <hackryme@gmail.com>
2022-10-23 20:18:57 -0600

    Nuevo
    -correcion fondo
    -correcion css
    -arreglo
    -optimizacion

--- js/model-viewer.js
+++ js/model-viewer.js
@@ -1,13 +1,30 @@
-const model = document.getElementById(model-earth); //el modelo 3d
+//SE ENCARGA DE LA RESOLUCION DINAMICA DEL VIZOR DEL MODELO
+//ajuista el modeo al ancho de la pantalla para que se mantenga en su lugar y redusca uso del gpu
+
+const model = document.getElementById("model-conteiner"); //el modelo 3d
+const modelViewer = document.querySelector("model-viewer#model-earth"); //el modelo 3d
 
 //inicializar
-let width, height, scale;
+let width, height, scale, newHeight, newWidth;
 //funcion
+//ajusta el modelo a la pantalla
 function FillScreen() {
-  width = window.innerWidth / canvas.width;
-  height = window.innerHeight / canvas.height;
-  scale = Math.min(h, w);
-  
-  canvas.style.width = (canvas.width * scale) + 'px';
-            canvas.style.height = (canvas.height * scale) + 'px';
+  //obtener la resolucion actual
+  width = window.innerWidth / model.clientWidth;
+  height = window.innerHeight / model.clientHeight;
+  //calcular [aspect ratio]
+  scale = Math.min(height, width);
+
+  //asignar nueva escala
+  newWidth = model.clientWidth * scale + "px";
+  //resolucion actual *15%
+  newHeight = model.clientHeight - (model.clientHeight*0.15) + "px";
+  model.style.width =newWidth;
+  model.style.height = newHeight;
+  //debug
+
+  console.log("La resolucion es: [" + newWidth + " X " + newHeight + "]");
 }
+
+window.addEventListener("resize", FillScreen);
+window.addEventListener("load", FillScreen);
