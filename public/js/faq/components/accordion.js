7867321761b5c8aa171768f5df182973e80dd7b8
Calvin Harris <garrixa6@gmail.com>
2022-10-24 23:29:25 -0500

    faqs y resources añadios

--- public/js/faq/components/accordion.js
+++ public/js/faq/components/accordion.js
@@ -8,6 +8,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
   for (let index = 0; index < accordionItem.length; index++) {
     accordionItem[index].addEventListener('click', ()=>{
       accordionContent[index].classList.toggle('open-accordion')
+      arrow[index].classList.toggle('arrow-transform')
     })
   }
 })
